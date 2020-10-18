6f9c2328c5fa1c429cc0a67b7f9c0e69751a542a
akise797 <prasetyo.akise@gmail.com>
2020-10-18 18:26:00 +0700

    Update Setting Auto Mapbender

--- setting_webserver_mapbender.sh
+++ setting_webserver_mapbender.sh
@@ -1,10 +1,11 @@
 # Update
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get update -y 
+sudo apt-get upgrade -y 
+sudo apt install -y software-properties-common
 
 # Install Firewall
 echo "Install Firewall"
-sudo apt-get install firewalld
+sudo apt-get install -y firewalld
 
 sudo systemctl enable firewalld
 sudo systemctl start firewalld
@@ -23,11 +24,11 @@ sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 
 # Update the package lists:
-sudo apt-get update
+sudo apt-get update -y 
 
 # Install the latest version of PostgreSQL.
 # If you want a specific version, use 'postgresql-12' or similar instead of 'postgresql':
-sudo apt install postgresql-9.6 postgresql-contrib-9.6 postgresql-9.6-postgis-scripts
+sudo apt install -y postgresql-9.6 postgresql-contrib-9.6 postgresql-9.6-postgis-scripts
 
 sudo sed -ie "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/9.6/main/postgresql.conf
 sudo sed -ie "s/#port = 5432/port = 5432/g" /etc/postgresql/9.6/main/postgresql.conf
@@ -42,7 +43,7 @@ sudo systemctl start postgresql
 
 # Install Apache 2 - Tomcat 8.5.59
 echo "Install Apache 2 - Tomcat 8.5.59"
-sudo apt-get install apache2 default-jdk
+sudo apt-get install -y apache2 default-jdk
 cd /tmp
 
 curl -O https://www-us.apache.org/dist/tomcat/tomcat-8/v8.5.59/bin/apache-tomcat-8.5.59.tar.gz
@@ -69,7 +70,7 @@ sudo systemctl start apache2
 
 # Install Geoserver 2.18.0
 echo "Install Geoserver 2.18.0"
-sudo apt-get update	
+sudo apt-get update	-y 
 
 cd /tmp
 
@@ -83,20 +84,30 @@ sudo systemctl restart apache2
 
 # Install Mapbender
 echo "Install Mapbender"
-sudo apt-get update	
+sudo apt-get update	-y 
 
 cd /tmp
 
-sudo sudo add-apt-repository ppa:ondrej/php
+sudo add-apt-repository ppa:ondrej/php -y
 
-sudo apt-get install php7.1 php7.1-pgsql libapache2-mod-php php7.1-common php7.1-gd php7.1-mysql php7.1-fpm php7.1-mcrypt php7.1-opcache php7.1-curl php7.1-cli php7.1-xml php7.1-sqlite3 sqlite3 php7.1-intl openssl php7.1-zip php7.1-mbstring php7.1-bz2 php7.1-xmlrpc php7.1-soap php7.1-bcmath php7.1-json php-pear
+sudo apt-get install -y php7.1 php7.1-pgsql libapache2-mod-php php7.1-common php7.1-ldap php7.1-gd php7.1-mysql php7.1-fpm php7.1-mcrypt php7.1-opcache php7.1-curl php7.1-cli php7.1-xml php7.1-sqlite3 sqlite3 php7.1-intl openssl php7.1-zip php7.1-mbstring php7.1-bz2 php7.1-xmlrpc php7.1-soap php7.1-bcmath php7.1-json php-pear
 
 wget https://mapbender.org/builds/mapbender-starter-current.tar.gz -O /var/www/mapbender-starter-current.tar.gz
 tar -zxf /var/www/mapbender-starter-current.tar.gz -C /var/www
 mv $(ls -d /var/www/*/ | grep mapbender) /var/www/mapbender/
 
-wget https://raw.githubusercontent.com/praswibowo/file_sh/master/Webserver%20Ubuntu%2018.04/tomcat.service
-sudo mv tomcat.service /etc/systemd/system/tomcat.service
+wget https://raw.githubusercontent.com/praswibowo/file_sh/master/Webserver%20Ubuntu%2018.04/mapbender.conf
+sudo mv mapbender.conf /etc/apache2/sites-available/mapbender.conf
 
-a2ensite mapbender.conf
-service apache2 reload
+sudo a2ensite mapbender.conf
+sudo service apache2 reload
+
+sudo chown -R www-data:www-data /var/www/mapbender/app/logs
+sudo chown -R www-data:www-data /var/www/mapbender/app/cache
+sudo chown -R www-data:www-data /var/www/mapbender/web/uploads
+
+sudo chmod -R ug+w /var/www/mapbender/app/logs
+sudo chmod -R ug+w /var/www/mapbender/app/cache
+sudo chmod -R ug+w /var/www/mapbender/web/uploads
+
+sudo chmod -R ug+w /var/www/mapbender/app/db/demo.sqlite
