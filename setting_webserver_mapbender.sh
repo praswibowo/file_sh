e811f0d0d85cdfe0dbf941d01a407a8d98c4bfd9
akise797 <prasetyo.akise@gmail.com>
2020-10-18 23:20:16 +0700

    Version 1.0.0 (tested)

--- setting_webserver_mapbender.sh
+++ setting_webserver_mapbender.sh
@@ -1,7 +1,7 @@
 # Update
 sudo apt-get update -y 
 sudo apt-get upgrade -y 
-sudo apt install -y software-properties-common
+sudo apt install -y software-properties-common unzip gzip wget
 
 # Install Firewall
 echo "Install Firewall"
@@ -28,7 +28,7 @@ sudo apt-get update -y
 
 # Install the latest version of PostgreSQL.
 # If you want a specific version, use 'postgresql-12' or similar instead of 'postgresql':
-sudo apt install -y postgresql-9.6 postgresql-contrib-9.6 postgresql-9.6-postgis-scripts
+sudo apt install -y postgresql-9.6 postgresql-contrib-9.6 postgresql-9.6-postgis-3-scripts
 
 sudo sed -ie "s/#listen_addresses = 'localhost'/listen_addresses = '*'/g" /etc/postgresql/9.6/main/postgresql.conf
 sudo sed -ie "s/#port = 5432/port = 5432/g" /etc/postgresql/9.6/main/postgresql.conf
@@ -41,17 +41,37 @@ sudo sed -ie "/host    all             all             127.0.0.1\/32            
 sudo systemctl enable postgresql
 sudo systemctl start postgresql
 
+echo "Install PGAdmin4"
+sudo apt install pgadmin4 pgadmin4-apache2
+
+echo "username : postgres@jarkom.net"
+echo "password : postgres"
+
+echo "Change Password Postgres 9.6"
+
+sudo sed -ie "s/local   all             postgres                                peer/local   all             postgres                                trust/g" /etc/postgresql/9.6/main/pg_hba.conf
+
+sudo systemctl reload postgresql
+
+sudo -u postgres psql -U postgres -d postgres -c "alter user postgres with password 'postgres';"
+
+echo "Change Password Is Done"
+
+sudo sed -ie "s/local   all             postgres                                trust/local   all             postgres                                md5/g" /etc/postgresql/9.6/main/pg_hba.conf
+
+sudo systemctl reload postgresql
+
 # Install Apache 2 - Tomcat 8.5.59
 echo "Install Apache 2 - Tomcat 8.5.59"
 sudo apt-get install -y apache2 default-jdk
 cd /tmp
 
-curl -O https://www-us.apache.org/dist/tomcat/tomcat-8/v8.5.59/bin/apache-tomcat-8.5.59.tar.gz
+wget https://www-us.apache.org/dist/tomcat/tomcat-8/v8.5.59/bin/apache-tomcat-8.5.59.tar.gz
 
 sudo useradd -m -U -d /opt/tomcat -s /bin/false tomcat
 
 sudo mkdir /opt/tomcat
-sudo tar xzvf apache-tomcat-8*tar.gz -C /opt/tomcat --strip-components=1
+sudo tar xzvf apache-tomcat-8.5.59.tar.gz -C /opt/tomcat --strip-components=1
 
 sudo chown -R tomcat: /opt/tomcat
 sudo sh -c 'chmod +x /opt/tomcat/bin/*.sh'
@@ -70,7 +90,7 @@ sudo systemctl start apache2
 
 # Install Geoserver 2.18.0
 echo "Install Geoserver 2.18.0"
-sudo apt-get update	-y 
+sudo apt-get update -y 
 
 cd /tmp
 
@@ -90,7 +110,11 @@ cd /tmp
 
 sudo add-apt-repository ppa:ondrej/php -y
 
-sudo apt-get install -y php7.1 php7.1-pgsql libapache2-mod-php php7.1-common php7.1-ldap php7.1-gd php7.1-mysql php7.1-fpm php7.1-mcrypt php7.1-opcache php7.1-curl php7.1-cli php7.1-xml php7.1-sqlite3 sqlite3 php7.1-intl openssl php7.1-zip php7.1-mbstring php7.1-bz2 php7.1-xmlrpc php7.1-soap php7.1-bcmath php7.1-json php-pear
+sudo apt-get install -y php7.1 php7.1-pgsql libapache2-mod-php7.1 php7.1-common php7.1-ldap php7.1-gd php7.1-mysql php7.1-fpm php7.1-mcrypt php7.1-opcache php7.1-curl php7.1-cli php7.1-xml php7.1-sqlite3 php7.1-intl openssl php7.1-zip php7.1-mbstring php7.1-bz2 php7.1-xmlrpc php7.1-soap php7.1-bcmath php7.1-json php-pear
+
+sudo a2enmod proxy_fcgi setenvif
+sudo a2enconf php7.1-fpm
+sudo systemctl restart apache2
 
 wget https://mapbender.org/builds/mapbender-starter-current.tar.gz -O /var/www/mapbender-starter-current.tar.gz
 tar -zxf /var/www/mapbender-starter-current.tar.gz -C /var/www
@@ -100,8 +124,10 @@ wget https://raw.githubusercontent.com/praswibowo/file_sh/master/Webserver%20Ubu
 sudo mv mapbender.conf /etc/apache2/sites-available/mapbender.conf
 
 sudo a2ensite mapbender.conf
+sudo a2enmod rewrite
 sudo service apache2 reload
 
+echo "Install Mapbender chmod/chown"
 sudo chown -R www-data:www-data /var/www/mapbender/app/logs
 sudo chown -R www-data:www-data /var/www/mapbender/app/cache
 sudo chown -R www-data:www-data /var/www/mapbender/web/uploads
@@ -111,3 +137,24 @@ sudo chmod -R ug+w /var/www/mapbender/app/cache
 sudo chmod -R ug+w /var/www/mapbender/web/uploads
 
 sudo chmod -R ug+w /var/www/mapbender/app/db/demo.sqlite
+
+echo "Checking Mapbender"
+cd /var/www/mapbender/
+sudo app/console mapbender:config:check
+
+echo "Install database Mapbender"
+
+sudo sed -ie "s/database_driver:   pdo_sqlite/database_driver:   pdo_pgsql/g" /var/www/mapbender/app/config/parameters.yml
+sudo sed -ie "s/database_host:     ~/database_host:     localhost/g" /var/www/mapbender/app/config/parameters.yml
+sudo sed -ie "s/database_port:     ~/database_port:     5432/g" /var/www/mapbender/app/config/parameters.yml
+sudo sed -ie "s/database_name:     ~/database_name:     mapbender/g" /var/www/mapbender/app/config/parameters.yml
+sudo sed -ie "s/database_path:     %kernel.root_dir%\/db\/demo.sqlite/database_path:     ~/g" /var/www/mapbender/app/config/parameters.yml
+sudo sed -ie "s/database_user:     ~/database_user:     postgres/g" /var/www/mapbender/app/config/parameters.yml
+sudo sed -ie "s/database_password: ~/database_password: postgres/g" /var/www/mapbender/app/config/parameters.yml
+
+echo "Install App Console Mapbender"
+
+sudo app/console doctrine:database:create
+sudo app/console doctrine:schema:create
+sudo app/console mapbender:database:init -v
+sudo app/console doctrine:fixtures:load --fixtures=./mapbender/src/Mapbender/CoreBundle/DataFixtures/ORM/Application/ --append
